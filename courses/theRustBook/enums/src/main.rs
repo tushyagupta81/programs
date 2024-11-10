791dbc51a857cbaed89e19d0ca271310075f5ee6
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-10 15:47:42 +0530

    the rust book

--- courses/theRustBook/enums/src/main.rs
+++ courses/theRustBook/enums/src/main.rs
@@ -1,20 +1,8 @@
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-}
-fn val_cents(coin: &Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-    }
-}
 fn main() {
-    let p = Coin::Penny;
-    let n = Coin::Nickel;
-    let d = Coin::Dime;
-    println!("{}", val_cents(&p));
-    println!("{}", val_cents(&n));
-    println!("{}", val_cents(&d));
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("Max is configed to {max}");
+    } else {
+        println!("Not configed");
+    }
 }
