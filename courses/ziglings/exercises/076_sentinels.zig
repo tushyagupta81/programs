52e1f26079f729a80b424335bb7554741a734da4
Tushya Gupta <tushyagupta81@gmail.com>
2025-01-29 21:01:52 +0530

    ziglings + os basic repo setup

--- courses/ziglings/exercises/076_sentinels.zig
+++ courses/ziglings/exercises/076_sentinels.zig
@@ -82,7 +82,7 @@ fn printSequence(my_seq: anytype) void {
             print("Array:", .{});
 
             // Loop through the items in my_seq.
-            for (???) |s| {
+            for (my_seq) |s| {
                 print("{}", .{s});
             }
         },
@@ -94,9 +94,8 @@ fn printSequence(my_seq: anytype) void {
             // Loop through the items in my_seq until we hit the
             // sentinel value.
             var i: usize = 0;
-            while (??? != my_sentinel) {
+            while (my_seq[i] != my_sentinel) : (i += 1) {
                 print("{}", .{my_seq[i]});
-                i += 1;
             }
         },
         else => unreachable,
