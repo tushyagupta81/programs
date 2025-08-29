ef39e02f47bc8dddca1163eaa589d27bf5388c88
Tushya Gupta <tushyagupta81@gmail.com>
2025-08-29 20:24:36 +0530

    removed more vunerable code - final for now

--- followAlong/nikeClone/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Button from "./Button.jsx";
-import Nav from "./Nav.jsx";
-import ShoeCard from "./ShoeCard.jsx";
-import PopularProductsCard from "./PopularProductsCard.jsx";
-import ServiceCard from "./ServiceCard.jsx";
-import ReviewCard from "./ReviewCard.jsx";
-
-export { ReviewCard, Button, Nav, ShoeCard, PopularProductsCard, ServiceCard };
