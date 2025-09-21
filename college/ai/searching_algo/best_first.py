b34203fb9fb720f7ed72082b84129ee3b3e1dff1
Tushya Gupta <tushyagupta81@gmail.com>
2025-09-21 12:58:00 +0530

    best first search

---
+++ college/ai/searching_algo/best_first.py
@@ -0,0 +1,35 @@
+import heapq
+
+graph = {
+    "A": [("B", 3), ("C", 5)],
+    "B": [("D", 4), ("E", 10)],
+    "C": [("F", 20), ("G", 9)],
+    "D": [],
+    "E": [],
+    "F": [],
+    "G": [],
+}
+
+
+def best_first(graph: dict[str, list[tuple[str, int]]], start: str, goal: str):
+    pq = [(0, start, [start])]
+    visited = set()
+
+    while pq:
+        _, node, path = heapq.heappop(pq)
+        print(f"{node} ", end="")
+
+        if node == goal:
+            return path
+        if node in visited:
+            continue
+
+        visited.add(node)
+
+        for n, cost in graph[node]:
+            heapq.heappush(pq, (cost, n, path + [n]))
+
+
+path = best_first(graph, "A", "E")
+print()
+print(f"Path = {path}")
